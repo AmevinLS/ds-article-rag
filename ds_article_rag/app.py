71daf0834a024d7020ee1a31052543367491bcb6
Nikita Makarevich <nikita_makarevich@yahoo.com>
2024-05-02 17:09:03 +0200

    Update regex code recognition. Change FAISS distance strategy to cosine. Update app.py to display matched paragraphs

--- ds_article_rag/app.py
+++ ds_article_rag/app.py
@@ -1,22 +1,25 @@
 import streamlit as st
+import pandas as pd
 
 from langchain.document_loaders.csv_loader import CSVLoader
 
 from retrieval import faiss_from_docs
 
+from typing import List
 
-def build_faiss(csv_path: str):
-    loader = CSVLoader(
-        csv_path, 
-        metadata_columns=["Title"], 
-        encoding="utf-8"
-    )
+
+@st.cache_resource
+def build_faiss(csv_path: str, metadata_columns: List[str]):
+    loader = CSVLoader(csv_path, metadata_columns=metadata_columns, encoding="utf-8")
     articles = loader.load()
 
-    return faiss_from_docs(
-        article_docs=articles,
-        model_name="all-MiniLM-L6-v2"
-    )
+    return faiss_from_docs(article_docs=articles, model_name="all-MiniLM-L6-v2")
+
+
+@st.cache_data
+def get_orig_articles(csv_path: str) -> pd.DataFrame:
+    art_df = pd.read_csv(csv_path, encoding="utf-8")
+    return art_df
 
 
 if __name__ == "__main__":
@@ -24,21 +27,32 @@ if __name__ == "__main__":
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
-    if "faiss_db" not in st.session_state:
-        st.session_state.faiss_db = build_faiss(R"D:\Repos\ds-article-rag\data\medium.csv")
+    messages: list = st.session_state.messages
 
-    for message in st.session_state.messages:
+    # TODO: perhaps build and persist faiss beforehand - and only load here
+    faiss_db = build_faiss(
+        R"D:\Repos\ds-article-rag\data\grouped_paragraphs.csv",
+        metadata_columns=["article_idx"],
+    )
+    art_df = get_orig_articles(R"D:\Repos\ds-article-rag\data\medium.csv")
+
+    ## Show previous messages
+    for message in messages:
         st.chat_message(message["role"]).markdown(message["content"])
 
-
+    ## Process user prompt
     if prompt := st.chat_input("Please write something"):
         st.chat_message("user").markdown(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
+        messages.append({"role": "user", "content": prompt})
 
-        docs = st.session_state.faiss_db.similarity_search(prompt, k=4)
-        response = "Relevant articles:"
+        docs = faiss_db.similarity_search(prompt, k=4)
+        response = "Relevant article segments:\n"
         for i, doc in enumerate(docs):
-            response += f"\n {i+1}. {doc.metadata['Title']}"
-        
+            response += (
+                f"#### {i+1}. {art_df.loc[int(doc.metadata['article_idx']), 'Title']}\n"
+            )
+            response += f"{doc.page_content[6:]}\n\n"
+            response += "-----\n\n"  # + "=" * 80
+
         st.chat_message("assistant").markdown(response)
-        st.session_state.messages.append({"role": "assisstant", "content": response})
+        messages.append({"role": "assisstant", "content": response})
--- ds_article_rag/retrieval.py
+++ ds_article_rag/retrieval.py
@@ -1,11 +1,16 @@
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.vectorstores.faiss import FAISS
-from langchain_community.docstore.document import Document
 
+from langchain_community.docstore.document import Document
+from langchain.vectorstores.utils import DistanceStrategy
 from typing import List
 
 
 def faiss_from_docs(article_docs: List[Document], model_name: str) -> FAISS:
     embeddings = HuggingFaceEmbeddings(model_name=model_name)
-    db = FAISS.from_documents(article_docs, embeddings)
+    db = FAISS.from_documents(
+        article_docs, 
+        embeddings, 
+        distance_strategy=DistanceStrategy.COSINE
+    )
     return db
